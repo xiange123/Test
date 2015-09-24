930e855552ae8c2c78a8838dc3b383416fc4c68c
xiange123 <263563240@qq.com>
2015-09-24 15:31:57 +0800

    777

---
+++ gitHub/gitHub/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitHub
+//
+//  Created by qianfeng on 15/9/24.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ gitHub/gitHub/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  gitHub
+//
+//  Created by qianfeng on 15/9/24.
+//  Copyright (c) 2015年 qianfeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //1231234567woaini 
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
